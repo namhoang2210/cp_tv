8a349fe661b4a69c9aa996f336ae7175b2b4653f
namhoang2210 <namle.22102000@gmail.com>
2022-11-07 03:16:05 +0700

    up date color bar chorme in mobie

--- src/components/Shared/Meta.tsx
+++ src/components/Shared/Meta.tsx
@@ -23,6 +23,7 @@ const Meta: FC<MetaProps> = ({ title, description, image }) => {
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
       <meta property="twitter:image" content={image} />
+      <meta name="theme-color" content="#032541" />
     </Head>
   );
 };

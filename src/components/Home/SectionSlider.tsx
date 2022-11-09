964bf9b2eff413af177b50d060625d8b62ab30a6
Hoang Nam <namle.22102000@gmail.com>
2022-11-09 23:07:38 +0700

    fix ui

--- src/components/Home/SectionSlider.tsx
+++ src/components/Home/SectionSlider.tsx
@@ -40,20 +40,22 @@ const Slider: FC<SliderProps> = ({ images }) => {
             key={item.image}
           >
             <Link href={item.link} prefetch={false} >
-              <a className="relative block rounded-lg overflow-hidden group ">
-                <ImageFade
-                  className="group-hover:brightness-75 transition duration-300 object-cover rounded-lg"
-                  src={item.image}
-                  width="150px"
-                  height="240px"
-                  alt=""
-                />
+              <a className="relative block rounded-lg overflow-hidden group">
+                <div className='bg-gray-200 w-[150px] h-[240px] rounded-lg'>
+                  <ImageFade
+                    className="group-hover:brightness-75 transition duration-300 object-cover rounded-lg"
+                    src={item.image}
+                    width="150px"
+                    height="240px"
+                    alt=""
+                  />
+                </div>
                 <h1 className="group-hover:text-[#48c1c4] transition duration-300 pt-4 text-sm font-semibold px-2 m-0 max-w-full whitespace-nowrap overflow-hidden text-ellipsis">
                   {item.title}
                 </h1>
                 {item.point && 
                   <CircularProgressbar
-                    className={`h-[32px] w-[32px] absolute bottom-7 left-2.5 rounded-full border-[2px] ${item.point >= 8 ? "bg-green-600 border-green-600" : item.point >= 5 ? "bg-[#e0a82e] border-[#e0a82e]" : "bg-red-400 border-red-400"}`}
+                    className={`h-[32px] w-[32px] absolute bottom-[22px] left-2.5 rounded-full border-[2px] ${item.point >= 8 ? "bg-green-600 border-green-600" : item.point >= 5 ? "bg-[#e0a82e] border-[#e0a82e]" : "bg-red-400 border-red-400"}`}
                     value={item.point * 10} 
                     text={`${item.point.toFixed(1)}`}/>
                 }
@@ -80,6 +82,7 @@ const Slider: FC<SliderProps> = ({ images }) => {
           >
             <Link href={item.link} prefetch={false} >
               <a className="block rounded-lg overflow-hidden group ">
+                <div className='bg-gray-200 h-[200px] rounded-lg'>
                 <ImageFade
                   className="group-hover:brightness-75 transition duration-300 object-cover rounded-lg"
                   src={item.image}
@@ -87,12 +90,14 @@ const Slider: FC<SliderProps> = ({ images }) => {
                   height="200px"
                   alt=""
                 />
+                </div>
+
                 <h1 className="group-hover:text-[#48c1c4] transition duration-300 pt-4 text-sm font-semibold px-1.5 lg:px-2 m-0 max-w-full whitespace-nowrap overflow-hidden text-ellipsis">
                   {item.title}
                 </h1>
                 {item.point && 
                   <CircularProgressbar
-                    className={`h-[32px] w-[32px] absolute bottom-7 left-2.5 rounded-full border-[2px] ${item.point >= 8 ? "bg-green-600 border-green-600" : item.point >= 5 ? "bg-[#e0a82e] border-[#e0a82e]" : "bg-red-400 border-red-400"}`}
+                    className={`h-[32px] w-[32px] absolute bottom-[22px] left-2.5 rounded-full border-[2px] ${item.point >= 8 ? "bg-green-600 border-green-600" : item.point >= 5 ? "bg-[#e0a82e] border-[#e0a82e]" : "bg-red-400 border-red-400"}`}
                     value={item.point * 10} 
                     text={`${item.point.toFixed(1)}`}/>
                 }

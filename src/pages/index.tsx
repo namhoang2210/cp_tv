6bbc014fb3f161ab44eb21eec85fd891acde2f32
namhoang2210 <namle.22102000@gmail.com>
2022-11-09 22:32:10 +0700

    add category

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -73,7 +73,7 @@ const Home: NextPage<HomeProps> = ({ topSearches }) => {
             ) : (
               <div key={section.homeSectionId} >
                 <div className="text-xl flex items-center gap-2 md:text-2xl mb-3 pt-12 relative">
-                  <div id={section.homeSectionId} className="absolute -top-10"></div>
+                  <div id={section.homeSectionId.toString()} className="absolute -top-10"></div>
                   <div className="font-medium md:font-semibold">{section.homeSectionName.replace("Loklok" || "LOKLOK", "trên ChuppyTV")}</div>
                 </div>
                 <SectionSlider
